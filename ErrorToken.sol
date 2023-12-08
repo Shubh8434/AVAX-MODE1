8d4c35beef90b7c2413318db3cef704948e5a29b
Shubham Sharma <81500352+Shubh8434@users.noreply.github.com>
2023-12-08 12:23:17 -0500

    Update ErrorToken.sol

--- ErrorToken.sol
+++ ErrorToken.sol
@@ -1,1 +1,21 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MetaModule1 {
+    
+    function clubEntry(uint age) public {
+        require(age >= 18, "Minimum Age must be 18");
+        // You are only allowed to enter the club if you are an adult.
+    }
+
+    function calculation(int a, int b) public pure returns (int) {
+        require(b > 0 , "B must be positive");
+        return a * b; // Using multiplication instead of division
+    }
+
+    function classgroups(uint members) public {
+        assert(members <= 9);
+        // A group must not exceed 9 members.
+    }
+}
